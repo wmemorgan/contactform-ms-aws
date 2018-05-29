c167529a3599f8e912e888a83c7c7e275ed662f9
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-05-29 12:28:56 +0800

    Customize contact form

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 'use strict';
-console.log('Loading function');
 const AWS = require('aws-sdk')
 AWS.config.region = 'us-east-1';
 const sesClient = new AWS.SES();
 const sesConfirmedAddress = "contact@wilfredmorgan.com";
 
-
-// /**
-//  * Lambda to process HTTP POST for contact form with the following body
-//  * {
-//       "email": <contact-email>,
-//       "subject": <contact-subject>,
-//       "message": <contact-message>
-//     }
-//  *
-//  */
-
 exports.handler = (event, context, callback) => {
-    console.log('Received event:', JSON.stringify(event, null, 2));
     var emailObj = event;
     var params = getEmailMessage(emailObj);
     var sendEmailPromise = sesClient.sendEmail(params).promise();
@@ -27,10 +14,8 @@ exports.handler = (event, context, callback) => {
     };
     
     sendEmailPromise.then(function(result) {
-        console.log(result);
         callback(null, response);
     }).catch(function(err) {
-        console.log(err);
         response.statusCode = 500;
         callback(null, response);
     });
